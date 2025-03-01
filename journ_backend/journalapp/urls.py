0a013224f6aba699cb78ce926d76847bdd8b58c7
Jonah Walker <jonah.h.walker@gmail.com>
2025-03-01 00:46:21 -0700

    AI URLS

--- journ_backend/journalapp/urls.py
+++ journ_backend/journalapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import JournalEntryViewSet, PromptResponsePairViewSet
-from .views import test_openai_api
+from .views import test_openai_api, generate_reflection_summary, generate_start_prompt, generate_further_prompt
 
 router = DefaultRouter()
 router.register(r'entries', JournalEntryViewSet)
@@ -9,5 +9,8 @@ router.register(r'prompts', PromptResponsePairViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('test-ai/', test_openai_api, name='test-openai_api')
+    path('test-ai/', test_openai_api, name='test-openai_api'),
+    path('reflect/', generate_reflection_summary, name='reflection-summary'),
+    path('start/', generate_start_prompt, name='start-prompt'),
+    path('further/', generate_further_prompt, name='further-prompt')
 ]
